23c19767ff6ca37b3a8be18346adedcb7194e3e2
Kyle Salstrom <kylesalstrom@gmail.com>
2020-01-06 17:06:46 -0800

    newer auth button

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -11,17 +11,32 @@ class GoogleAuth extends React.Component {
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
 
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in</div>;
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in</div>;
+            return (
+                <button className="ui red google button">
+                <i className="google icon" />
+                Sign Out
+            </button>
+            );
         } else {
-            return <div>I am not signed in</div>;
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Google Sign In
+            </button>
+            );
         }
     }
 
