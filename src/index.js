581f4515afb80937d6dcea6147b9afe7e454e359
Kyle Salstrom <kylesalstrom@gmail.com>
2020-01-07 17:05:29 -0800

    wires in some redux and redux-form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDom.render(<App />, document.querySelector('#root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+);
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+        document.querySelector('#root')
+);
